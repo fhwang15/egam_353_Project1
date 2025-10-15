8e13dab347ca849d512dc9494c2beafbcfc35660
fhwang15 <fhwang@inside.artcenter.edu>
2025-10-16 04:20:23 +0900

    dsad
    fsdafd

---
+++ Assets/Scripts/Data/recipe.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class recipe
+{
+    public int recipeID;
+    public string name;
+    public string description;
+
+    public bool specialWokTossing;
+    public int wokTossingNumber;
+
+    public int requiredTemp;
+    public int cookingTime;
+}
